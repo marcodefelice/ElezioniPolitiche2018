ad0b9c74d3e68dd3938760f08ca37a9d2058c8f4
matteo <matteo.fiorio.vr@gmail.com>
2018-01-21 15:02:18 +0100

    resolveing merge

--- src/common/containers/App/index.js
+++ src/common/containers/App/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 export default () => (
     <Fragment>
